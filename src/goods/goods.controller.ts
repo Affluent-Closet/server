274b4fcece9c4fabcbea9b9d0caadec1ee9affd2
chujonghyun <chuwhdgus@gmail.com>
2022-02-16 20:52:35 +0900

    goods CRUD 프로토타입 구현

--- src/goods/goods.controller.ts
+++ src/goods/goods.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { CreateGoodsDto } from './dto/create-goods.dto';
 import { Goods } from './entities/goods.entity';
 import { GoodsService } from './goods.service';
@@ -12,5 +20,30 @@ export class GoodsController {
   @Post('')
   createGoods(@Body() createGoodsDto: CreateGoodsDto): Promise<Goods> {
     return this.goodsService.createGoods(createGoodsDto);
+  }
+
+  //(Get) localhost:3000/goods
+  @Get('')
+  getAllGoods(): Promise<Goods[]> {
+    return this.goodsService.getAllGoods();
+  }
+
+  //(Get) localhost:3000/goods/:id
+  @Get('/:id')
+  getGoodsById(@Param('id', ParseIntPipe) id: number): Promise<Goods> {
+    return this.goodsService.getGoodsById(id);
+  }
+
+  // @Post('/:id/update')
+  // updateGoods(
+  //   @Param('id') id:number,
+  //   @Body('goods')
+  // ):Promise<Goods>{
+  //   return this.goodsService.updateGoods(id, )
+  // }
+
+  @Delete('/:id')
+  deleteGoods(@Param('id', ParseIntPipe) id: number) {
+    return this.goodsService.deleteGoods(id);
   }
 }
--- src/goods/goods.service.ts
+++ src/goods/goods.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateGoodsDto } from './dto/create-goods.dto';
 import { Goods } from './entities/goods.entity';
@@ -11,7 +11,55 @@ export class GoodsService {
     private goodsRepository: GoodsRepository,
   ) {}
 
+  //goods 생성
   createGoods(createGoodsDto: CreateGoodsDto): Promise<Goods> {
     return this.goodsRepository.createGoods(createGoodsDto);
+  }
+
+  //모든 goods 가져오기
+  async getAllGoods(): Promise<Goods[]> {
+    const goods = this.goodsRepository.find();
+    return goods;
+  }
+
+  //특정 goods 가져오기
+  async getGoodsById(id: number): Promise<Goods> {
+    const found = this.goodsRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException(
+        `${id}로 요청한 상품을 찾을 수 없습니다. 아이디를 다시 한번 확인해 주세요.`,
+      );
+    }
+
+    return found;
+  }
+
+  async updateGoods(id: number, newGoods: Goods): Promise<Goods> {
+    const goods = await this.getGoodsById(id);
+
+    goods.category = newGoods.category;
+    goods.detail = newGoods.detail;
+    goods.discount = newGoods.discount;
+    goods.name = newGoods.name;
+    goods.price = newGoods.price;
+    // goods.sellnum = newGoods.sellnum;
+    // goods.stock = newGoods.stock;
+    goods.thumbnail = newGoods.thumbnail;
+
+    await this.goodsRepository.save(goods);
+
+    return goods;
+  }
+
+  async deleteGoods(id: number): Promise<void> {
+    const result = await this.goodsRepository.delete({ id });
+
+    //id로 찾은 데이터가 디비에 없을 때
+    if (result.affected === 0) {
+      throw new NotFoundException(
+        `${id}로 찾은 goods가 DB에 없습니다. id를 다시 한번 확인해 주세요.`,
+      );
+    }
   }
 }
