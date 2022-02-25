c1d4841b5d7c39789250a01c64a0840d2472e1a5
chujonghyun <chuwhdgus@gmail.com>
2022-02-25 16:53:55 +0900

    환경변수 세팅

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,11 +1,9 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// //aws 설정 다 해준 상태
-
 export const typeORMconfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST || 'localhost',
-  port: 5432,
+  port: parseInt(process.env.POSTGRES_PORT) || 5432,
   username: process.env.POSTGRES_USERNAME || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: process.env.POSTGRES_DATABASE || 'goods',
