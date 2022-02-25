3383048e57d185c534a16f836492e00a96f3f44b
chujonghyun <chuwhdgus@gmail.com>
2022-02-25 15:13:29 +0900

    s3 연동 및 이미지 업로드 기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMconfig } from './configs/typeorm.config';
 import { GoodsModule } from './goods/goods.module';
+import { ImageModule } from './image/image.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMconfig), GoodsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMconfig),
+    GoodsModule,
+    ImageModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
