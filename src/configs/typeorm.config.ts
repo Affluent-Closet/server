dd479a1dcc6ad81fc12593bc67660fa85255eb0c
추종현 <67010993+jonghyunhub@users.noreply.github.com>
2022-02-28 20:15:09 +0900

    Revert "port 변수 세팅"

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeORMconfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST || 'localhost',
-  port: process.env.PORT || 5432,
+  port: parseInt(process.env.POSTGRES_PORT) || 5432,
   username: process.env.POSTGRES_USERNAME || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: process.env.POSTGRES_DATABASE || 'goods',
