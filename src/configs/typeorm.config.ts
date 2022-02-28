072b59c3e1a5fd5800a221d2b092645c098bea62
chujonghyun <chuwhdgus@gmail.com>
2022-02-28 20:23:04 +0900

    third commit

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeORMconfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST || 'localhost',
-  port: (process.env.PORT as any) || 5432,
+  port: 5432,
   username: process.env.POSTGRES_USERNAME || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: process.env.POSTGRES_DATABASE || 'goods',
