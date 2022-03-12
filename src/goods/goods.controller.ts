2189495d914cb699d81a7b7b1336318ce6374e06
chujonghyun <chuwhdgus@gmail.com>
2022-03-12 21:50:04 +0900

    다시 원상 복구

--- src/goods/goods.controller.ts
+++ src/goods/goods.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { CreateGoodsDto } from './dto/create-goods.dto';
 import { UpdateGoodsDto } from './dto/update-goods.dto';
-import { UploadImageGoodsDTO } from './dto/uploadImage-goods.dto';
 import { Goods } from './entities/goods.entity';
 import { GoodsService } from './goods.service';
 
@@ -44,7 +43,7 @@ export class GoodsController {
   @Post('/:id/update')
   updateGoods(
     @Param('id') id: number,
-    @Body() updateGoodsDto: UploadImageGoodsDTO,
+    @Body() updateGoodsDto: UpdateGoodsDto,
   ): Promise<Goods> {
     return this.goodsService.updateGoods(id, updateGoodsDto);
   }
