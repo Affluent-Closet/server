41aeb494837d0abe5682be2646de1ae192735ef5
chujonghyun <chuwhdgus@gmail.com>
2022-05-18 22:32:28 +0900

    결제 함수 주석 추가

--- src/pay/pay.controller.ts
+++ src/pay/pay.controller.ts
@@ -5,9 +5,8 @@ import { PayService } from './pay.service';
 @Controller('pay')
 export class PayController {
   constructor(private payService: PayService) {}
-  //   @Get()
-  //   getPay(@Res() res) {}
 
+  /**토스 페이먼츠 api에 결제 요청하는 함수 */
   @Post('/success')
   successPay(@Body() req: PayQueryDto) {
     console.log(req);
