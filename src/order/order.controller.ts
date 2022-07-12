667ebc34d5a60487f379ab7d34d7b054e598061f
chujonghyun <chuwhdgus@gmail.com>
2022-07-12 21:19:17 +0900

    주문생성 컨트롤러 메서드 구현

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { OrderService } from './order.service';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Post()
+  createOrder(@Body() createOrderDto: CreateOrderDto) {
+    await this.orderService.createOrder(createOrderDto);
+  }
+}
